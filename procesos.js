1fc8f2afe489234b0f98268922803f86299b21e4
JessCas16 <jessycas.g16@gmail.com>
2022-12-11 17:09:59 -0400

    prueba commit 2

--- procesos.js
+++ procesos.js
@@ -2,3 +2,12 @@ const express = require("express")
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || '3000'
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+    console.log("Escuchando el puerto " + port)
+})
